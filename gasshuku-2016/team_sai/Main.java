375f77da5b7f4a54d12be9342a354c2d871ac77c
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-09-08 08:34:52 +0900

    team sai

---
+++ gasshuku-2016/team_sai/Main.java
@@ -0,0 +1,26 @@
+package team_sai;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static List<Integer> studentNumber = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 30; i++) {
+            studentNumber.add(i + 1);
+        }
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            for (int i = 0; i < 28; i++) {
+                int n = sc.nextInt();
+                int a = studentNumber.indexOf(n);
+                studentNumber.remove(a);
+            }
+            for (int i = 0; i < studentNumber.size(); i++) {
+                System.out.println(studentNumber.get(i));
+            }
+        }
+    }
+}
